60f6c02166322c11c39617b1715606abdd83307b
131220070-Zhou Yaoyang <diamondzyy@sina.com>
2016-09-19 11:06:29 +0800

    add history log

--- scrap.py
+++ scrap.py
@@ -4,7 +4,7 @@
 from bs4 import BeautifulSoup
 import urllib2
 import urllib
-import re
+import time
 from mail import send
 
 
@@ -13,7 +13,7 @@ mail_receiver = '1058149101@qq.com'
 target_url_head = 'https://s.2.taobao.com/list/list.htm?q='
 target_url_tail = '&search_type=item&app=shopsearch'
 
-test = True
+test = False
 
 
 def gen_req(url):
@@ -29,12 +29,17 @@ def gen_req(url):
     return req
 
 
-def scrap_page(url, expected_price):
+def link_to_id(link):
+    return int(link.split('id=')[1])
+
+
+def scrap_page(url, expected_price, history_list):
     req = gen_req(url)
     r = urllib2.urlopen(req).read()
     soup = BeautifulSoup(r, "html.parser")
     # print soup.prettify()
     lis = soup.find_all('li', class_='item-info-wrapper item-idle clearfix')
+    item_list = []
     for li in lis:
         # print li
         price_div = li.findChild('div', class_="item-price price-block")
@@ -47,11 +52,15 @@ def scrap_page(url, expected_price):
         a = li.findChild('a')
         link = 'http:' + a['href']
         title = unicode(a.findChild('img')['alt'])
+        if link_to_id(link) in history_list:
+            continue
+        item_list.append(link_to_id(link))
         if test:
             print title
             print link
         else:
             send(mail_sender, mail_receiver, title+'\n'+link)
+    return item_list
 
 
 expected_prices = dict()
@@ -61,8 +70,27 @@ expected_prices['樱桃茶轴'] = 120
 expected_prices['樱桃青轴'] = 120
 
 
-for k in expected_prices:
-    item = urllib.quote(k.decode('utf-8').encode('gbk'))
-    print k, item
-    print target_url_head+item+target_url_tail
-    scrap_page(target_url_head+k+target_url_tail, expected_prices[k])
+def main():
+    while True:
+        try:
+            history_list = []
+            # read history
+            with open('history.txt') as f:
+                for line in f:
+                    history_list.append(int(line))
+            for k in expected_prices:
+                item = urllib.quote(k.decode('utf-8').encode('gbk'))
+                print '正在爬取', k, 'Escaped:', item
+                # print target_url_head+item+target_url_tail
+                new_list = scrap_page(target_url_head+k+target_url_tail, expected_prices[k], history_list)
+                if new_list:
+                    with open('history.txt', 'a') as f:
+                        for line in new_list:
+                            print >>f, line
+        except Exception as e:
+            print e
+        time.sleep(10)
+
+
+if __name__ == '__main__':
+    main()
