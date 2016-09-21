52f739903e79b58d86c834ac9caadd71a37ca6be
Zhou Yaoyang <diamondzyy@sina.com>
2016-09-21 16:19:56 +0800

    add more items and randomized access time interval

--- scrap.py
+++ scrap.py
@@ -6,6 +6,7 @@ import urllib2
 import urllib
 import time
 from mail import send
+import random
 
 
 mail_sender = 'diamondzyy@163.com'
@@ -14,7 +15,7 @@ target_url_head = 'https://s.2.taobao.com/list/list.htm?' \
         'spm=2007.1000337.6.2.x7MnnJ&st_edtime=1&q='
 target_url_tail = '&ist=0'
 
-test = False
+test = True
 
 
 def gen_req(url):
@@ -39,7 +40,10 @@ black_list = [
     u'坏',
     u'收',
     u'尸体',
-    u'华为'
+    u'华为',
+    u'求购',
+    u'主板',
+    u'计算器',
 ]
 
 
@@ -83,39 +87,79 @@ def scrap_page(url, expected_price, history_list):
 
 
 expected_prices = dict()
-expected_prices['fx4300'] = 230
-expected_prices['fx6300'] = 335
 expected_prices['樱桃茶轴'] = 120
 expected_prices['樱桃青轴'] = 120
 expected_prices['魅族mx2'] = 120
 expected_prices['魅族mx3'] = 200
 expected_prices['魅蓝'] = 250
 
-raw_1155 = '''G530 = 100
-                 G540 = 110
-    G620 = 110
-                 G630 = 130
-                G640 = 140
-                G1620 = 160
-                I3 2100 = 300
-I3 2120 = 310
-I3 2130 = 310
-                  I3 3220 = 370
-I3 3240 = 380
-                  I5 2300 = 480
-                   I5 2320 = 500
-                   I5 2400 = 530
-                             I5 3470 = 660'''
+raw_1155 = '''
+G530=100
+G540=110
+G620=110
+G630=130
+G640=140
+G1620=160
+I3-2100=300
+I3-2120=310
+I3-2130=310
+I3-3220=370
+I3-3240=3870
+I5-2300=480
+I5-2320=500
+I5-2400=530
+I5-3470=660
+'''
+
+raw_1150 = '''
+G3258=280
+i3-4130=570
+i3-4150=585
+i3-4160=605
+i3-4170=615
+'''
+
+raw_amd = '''
+X4 631=50
+X4 641=60
+X4 651=70
+X4 620=140
+X4 630=150
+X4 635=150
+X4 640=170
+X4 645=175
+X4 740=160
+X4 750=170
+X4 955=180
+X4 965=210
+1055T=310
+1100T=700
+FX 4300=255
+FX 6100=300
+FX 6200=340
+FX 6300=360
+FX 8100=470
+FX 8120=510
+FX 8150=520
+FX 8320=580
+FX 8350=750
+'''
 
 
 def preproc_price():
     prices_1155 = raw_1155.split('\n')
-    for line in prices_1155:
-        item, price = line.lstrip(' ').rstrip(' ').split(' = ')
+    prices_1150 = raw_1150.split('\n')
+    prices_amd = raw_amd.split('\n')
+    prices = prices_1155 + prices_1150 + prices_amd
+    for line in prices:
+        if not len(line):
+            continue
+        item, price = line.lstrip(' ').rstrip(' ').split('=')
         item = item.replace(' ', '+')
-        price = int(price)
+        item = item.replace('-', '+')
+        price = int(price) - 20
         print item, price
-        expected_prices[item] = price - 25
+        expected_prices[item] = price
 
 
 def main():
@@ -131,19 +175,21 @@ def main():
                 item = urllib.quote(k.decode('utf-8').encode('gbk'))
                 print '正在爬取', k, 'Escaped:', item, 'Expected Price:', expected_prices[k]
                 if test:
-                    print target_url_head+item+target_url_tail
+                    print target_url_head + item + target_url_tail
                 new_list = scrap_page(target_url_head+k+target_url_tail,
                                       expected_prices[k], history_list)
+                time.sleep(random.randint(1, 8))
                 if new_list:
                     with open('history.txt', 'a') as f:
                         for line in new_list:
                             print >>f, line
         except Exception as e:
             print e
+        print '-------------------------------------------------------'
         if test:
-            time.sleep(5)
+            time.sleep(1)
         else:
-            time.sleep(600)
+            time.sleep(300)
 
 
 if __name__ == '__main__':
